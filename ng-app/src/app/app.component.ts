b7fb8d9bac06d246c68dcd2982e3d3cb22b4b2fe
Omar Momani <o.a.momani@gmail.com>
2022-06-13 23:36:18 +0300

    NgbModule added

--- ng-app/src/app/app.component.ts
+++ ng-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbCarouselConfig],
 })
 export class AppComponent {
   title = 'ng-app';
+  images = [700, 533, 807, 124, 456].map(
+    (n) => `https://picsum.photos/id/${n}/900/500`
+  );
+  constructor(config: NgbCarouselConfig) {
+    config.wrap = true;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 }
--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  entryComponents: [NgbModule],
+  exports: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
