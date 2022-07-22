c2e75393ee014938189a18930fbde010f9c5a7bf
Omar Momani <o.a.momani@gmail.com>
2022-07-22 07:42:55 +0300

    Fixing login page

--- ng-app/src/app/auth/auth.service.ts
+++ ng-app/src/app/auth/auth.service.ts
@@ -125,18 +125,8 @@ export class AuthService {
     let errorMessage = 'An unknown error occurred!';
     if (!errorRes.error || !errorRes.error.error) {
       return throwError(() => new Error(errorMessage));
-    }
-    switch (errorRes.error.error.message) {
-      case 'EMAIL_EXISTS':
-        errorMessage = 'This email exists already';
-        break;
-      case 'EMAIL_NOT_FOUND':
-        errorMessage = 'This email does not exist.';
-        break;
-      case 'INVALID_PASSWORD':
-        errorMessage = 'This password is not correct.';
-        break;
-    }
-    return throwError(() => new Error(errorMessage));
+    } else errorMessage = errorRes.error.error.message;
+
+    return throwError(() => new Error(errorMessage.replace(/_+/g, ' ')));
   }
 }
